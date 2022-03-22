83411a1c10ecc998e1b95eaf3704b6b0a160a4cf
Jade Ghadry <jadghadry@gmail.com>
2022-03-22 21:04:28 +0100

    chore: add the `Footer` component

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+import { ResponsiveContainer } from "_/wrappers/ResponsiveContainer";
+
+export type FooterProps = {};
+
+export const Footer = ({}: FooterProps) => {
+  return <ResponsiveContainer style={{ backgroundColor: "#000" }}></ResponsiveContainer>;
+};
