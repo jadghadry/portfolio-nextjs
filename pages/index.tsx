561929a1452874a45797f2777b15026373076b58
Jade Ghadry <jadghadry@gmail.com>
2022-03-22 20:53:32 +0100

    chore: implement the initial Home page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,43 @@
 import type { NextPage } from "next";
+import { FlexContainer } from "_/components/FlexContainer";
+import { OverlayedParallax } from "_/wrappers/OverlayedParallax";
+import { ResponsiveContainer } from "_/wrappers/ResponsiveContainer";
 
 const Index: NextPage = () => {
-  return null;
+  return (
+    <>
+      <section id="landing">
+        <OverlayedParallax
+          backgroundImage="https://source.unsplash.com/tMbQpdguDVQ/2400x1600"
+          styles={{
+            container: {
+              height: "100vh",
+            },
+          }}
+        >
+          <FlexContainer centered flexDirection="column" style={{ height: "100%" }}></FlexContainer>
+        </OverlayedParallax>
+      </section>
+      <section id="about">
+        <OverlayedParallax backgroundImage="https://source.unsplash.com/pVt9j3iWtPM/2400x1600">
+          <FlexContainer centered flexDirection="column" style={{ height: "100%" }}></FlexContainer>
+        </OverlayedParallax>
+        <ResponsiveContainer></ResponsiveContainer>
+      </section>
+      <section id="resume">
+        <OverlayedParallax backgroundImage="https://source.unsplash.com/bUpwY7EdrlQ/2400x1600">
+          <FlexContainer centered flexDirection="column" style={{ height: "100%" }}></FlexContainer>
+        </OverlayedParallax>
+        <ResponsiveContainer></ResponsiveContainer>
+      </section>
+      <section id="services">
+        <OverlayedParallax backgroundImage="https://source.unsplash.com/aOC7TSLb1o8/2400x1714">
+          <FlexContainer centered flexDirection="column" style={{ height: "100%" }}></FlexContainer>
+        </OverlayedParallax>
+        <ResponsiveContainer></ResponsiveContainer>
+      </section>
+    </>
+  );
 };
 
 export default Index;
