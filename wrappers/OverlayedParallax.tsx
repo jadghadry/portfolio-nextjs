e496e3d77503973c23c679a14c7f2470a2e35d17
Jade Ghadry <jadghadry@gmail.com>
2022-03-22 20:52:56 +0100

    chore: add the `OverlayedParallax` wrapper for the `Parallax` component

---
+++ wrappers/OverlayedParallax.tsx
@@ -0,0 +1,22 @@
+import { CSSProperties, PropsWithChildren } from "react";
+import { Parallax, ParallaxProps } from "_/components/Parallax";
+import { ResponsiveContainer } from "./ResponsiveContainer";
+
+export type OverlayedParallaxProps = PropsWithChildren<
+  ParallaxProps & {
+    overlayColor?: string | undefined;
+    styles?: {
+      container?: CSSProperties | undefined;
+    };
+  }
+>;
+
+export const OverlayedParallax = ({ backgroundImage, children, overlayColor = "#00000080", styles }: OverlayedParallaxProps) => {
+  return (
+    <Parallax backgroundImage={backgroundImage} style={styles?.container}>
+      <ResponsiveContainer style={{ backgroundColor: overlayColor, color: "#fff", height: "100%" }}>
+        {children}
+      </ResponsiveContainer>
+    </Parallax>
+  );
+};
