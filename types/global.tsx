53f18547c75268916117234c5947796a1b87e7c1
Jade Ghadry <jadghadry@gmail.com>
2022-03-23 16:30:33 +0100

    chore: add the types directory

---
+++ types/global.tsx
@@ -0,0 +1,14 @@
+export type Service = {
+  description: string;
+  icon: string;
+  id: string;
+  title: string;
+};
+
+export type TimelineEntry = {
+  bulletPoints: string[];
+  endDate: string | null;
+  id: string;
+  startDate: string;
+  title: string;
+};
